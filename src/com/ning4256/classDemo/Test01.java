7f1d475f7689f9510c3ae7b51211079145aee1dd
xiaofandegeng <478465454@qq.com>
2019-02-18 14:34:42 +0800

    反射构造一个对象

--- src/com/ning4256/classDemo/Test01.java
+++ src/com/ning4256/classDemo/Test01.java
@@ -1,25 +1,44 @@
 package com.ning4256.classDemo;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.ning4256.genritic.GeneriticDemo;
 import com.ning4256.po.UserPo;
 
 public class Test01 {
-	public static UserPo encap(Map<?, ?> map, Class<?> class1) throws Exception {
-		UserPo userPo = (UserPo) class1.newInstance();
+	public static <T> T enca(Map<?, ?> map, Class<T> class1) throws Exception {
+
+		T t = null;
+		// 非空判断
+		if (map == null || class1 == null) {
+			return t;
+		}
+		// 创建对象
+		t = (T) class1.newInstance();
+		// 操作属性
+		// 获取本类中的所有属性
 		Field[] fields = class1.getDeclaredFields();
 		for (int i = 0; i < fields.length; i++) {
 			fields[i].setAccessible(true);
-			fields[i].set(userPo, map.get(fields[i].getName()));
+			fields[i].set(t, map.get(fields[i].getName()));
 		}
-		return userPo;
+		return t;
 	}
+
 	public static void main(String[] args) throws Exception {
-		HashMap< String, Object> map = new HashMap<>();
-		map.put("id", 2);
-		Class<?> class1 = Class.forName("com.ning4256.po.UserPo");
-		System.out.println(encap(map, class1));
+		Map<String, Object> map = new HashMap<>();
+		map.put("pass", 1234);
+		map.put("id", 1);
+		map.put("name", "伍川");
+		
+		Class<UserPo> class1 = (Class<UserPo>) Class.forName("com.ning4256.po.UserPo");
+		System.out.println(enca(map, class1));
+
+		Method mainMethod = GeneriticDemo.class.getMethod("main", String[].class);
+		mainMethod.invoke(null, (Object) new String[] { "a", "b", "c" });
+
 	}
 }
